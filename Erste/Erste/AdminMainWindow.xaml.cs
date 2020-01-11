dfd724c9f87ca6708aa12f99b69bdd4851db2b9a
marijana97etf <marijana97zeljkovic@gmail.com>
2020-01-11 01:37:26 +0100

    Kreirane glavnih formi.

---
+++ Erste/Erste/AdminMainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Erste
+{
+    /// <summary>
+    /// Interaction logic for AdminMainWindow.xaml
+    /// </summary>
+    public partial class AdminMainWindow : Window
+    {      
+        public AdminMainWindow()
+        {           
+            InitializeComponent();
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }  
+    }
+}
---
+++ Erste/Erste/SluzbenikMainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Erste
+{
+    /// <summary>
+    /// Interaction logic for SluzbenikMainWindow.xaml
+    /// </summary>
+    public partial class SluzbenikMainWindow : Window
+    {      
+        public SluzbenikMainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }  
+    }
+}
