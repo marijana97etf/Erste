e2395f5ff9ba5650ed44c599e0e4807af590f88f
David Baralic <34375873+BaralicDavid@users.noreply.github.com>
2020-02-19 19:03:15 +0100

    Delete profesori.sql

--- Baza podataka/skripte/profesori.sql
+++
@@ -1,5 +0,0 @@
-insert into osoba(Id,Ime,Prezime,Email,BrojTelefona) values(3,"Jovana","Jovanovic","jovana.jovanovic","065/000-000");
-insert into osoba(Id,Ime,Prezime,Email,BrojTelefona) values(4,"Mira","Mirkovic","mira.mirkovic","065/000-000");
-
-insert into profesor(Id) values(3);
-insert into profesor(Id) values(4);
