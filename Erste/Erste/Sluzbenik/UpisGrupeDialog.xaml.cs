4ef8e94ac7225e063016d1315e4e6ed7a5fc0464
David Baralic <34375873+BaralicDavid@users.noreply.github.com>
2020-02-20 06:31:34 +0100

    Uradjeno sve za upis kandidata,s nekim neobicnim bagom,dodati combo box-ovi

---
+++ Erste/Erste/Sluzbenik/UpisGrupeDialog.xaml.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Erste.Sluzbenik
+{
+    /// <summary>
+    /// Interaction logic for UpisTerminaGrupe.xaml
+    /// </summary>
+    public partial class UpisTerminaGrupe : Window
+    {
+        public grupa Grupa { set; get; }
+        public string NazivGrupe { set; get; }
+
+        public UpisTerminaGrupe(grupa g)
+        {
+            InitializeComponent();
+            Grupa = g;
+        }
+
+        private void Button_Otkazi_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Button1Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(textBox_NazivGrupe.Text))
+            {
+                MessageBox.Show("Odaberite naziv grupe.");
+                return;
+            }
+            if (string.IsNullOrWhiteSpace(DanCombo.Text))
+            {
+                MessageBox.Show("Odaberite dan termina.");
+                return;
+            }
+            if (string.IsNullOrWhiteSpace(TimePickerOd.Value.ToString()) || string.IsNullOrWhiteSpace(TimePickerDo.Value.ToString()))
+            {
+                MessageBox.Show("Popunite termine.");
+                return;
+            }
+            Func<DateTime?, DateTime?, bool> compare = (a, b) => a?.TimeOfDay.CompareTo(b?.TimeOfDay) > 0;
+            if (compare(TimePickerOd.Value, TimePickerDo.Value))
+            {
+                MessageBox.Show("Termin početka mora biti prije termina završetka.");
+                return;
+            }
+
+            try
+            { 
+                TimeSpan @od = new TimeSpan();
+                TimeSpan @do = new TimeSpan();
+                string dan = "";
+                if (TimePickerOd.Value == null || TimePickerDo.Value == null || DanCombo.SelectedValue == null) return;
+                dan = DanCombo.Text;
+                @od = TimePickerOd.Value.Value.TimeOfDay;
+                @do = TimePickerDo.Value.Value.TimeOfDay;
+
+                using (ErsteModel ersteModel = new ErsteModel())
+                {
+                    termin t = new termin()
+                    {
+                        Dan = dan,
+                        Od = @od,
+                        Do = @do,
+                        grupa = Grupa,
+                        GrupaId = Grupa.Id
+                    };
+                    ersteModel.termini.Add(t);
+                    Grupa.termini.Add(t);
+                    Grupa.Naziv = textBox_NazivGrupe.Text;
+                    ersteModel.SaveChanges();
+                }
+                MessageBox.Show("Uspješno ste dodali novi termin.");
+            }
+            catch (IOException ioException)
+            {
+                MessageBox.Show(ioException.StackTrace);
+            }
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
--- Erste/Erste/Sluzbenik/UpisPolaznikaDialog.xaml.cs
+++ Erste/Erste/Sluzbenik/UpisPolaznikaDialog.xaml.cs
@@ -1,16 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace Erste.Sluzbenik
 {
@@ -22,85 +15,83 @@ namespace Erste.Sluzbenik
         public UpisPolaznikaDialog()
         {
             InitializeComponent();
+            loadChb();
         }
 
-        private async void btn_PrikaziGrupe_Click(object sender, RoutedEventArgs e)
+        private void loadChb()
         {
-            if (string.IsNullOrEmpty(textBox_Jezik.Text) && string.IsNullOrEmpty(textBox_NivoKursa.Text))
+
+            using (var ersteModel = new ErsteModel())
             {
-                if (Dispatcher != null)
-                    await Dispatcher?.InvokeAsync(() =>
-                    {
-                        NapomenaBox.Visibility = Visibility.Visible;
-                        NapomenaBox.Background = new SolidColorBrush(Color.FromArgb(0xFF, 0xEF, 0x3D, 0x4A));
-                        NapomenaBox.Text = "Unesite jezik i nivo kursa za prikaz grupa.";
-                    });
+                var jezici = (from j in ersteModel.jezici select j).Select(j => j.Naziv);
+                var nivoi = (from k in ersteModel.kursevi select k).GroupBy(k => k.Id).Select(k => k.FirstOrDefault()).Select(k => k.Nivo);
+
+               foreach(string naziv in jezici)
+                    chb_Jezik.Items.Add(naziv);
+               foreach( string nivo in nivoi)
+                    chb_Nivo.Items.Add(nivo);
+            }
+        }
+
+        private void btn_PrikaziGrupe_Click(object sender, RoutedEventArgs e)
+        {
+            string odabraniJezik = (string)chb_Jezik.SelectedItem;
+            string odabraniNivo = (string)chb_Nivo.SelectedItem;
+
+            if (string.IsNullOrEmpty(odabraniJezik) && string.IsNullOrEmpty(odabraniNivo))
+            {
+                MessageBox.Show("Odaberite jezik i nivo kursa za prikaz grupa.");
                 return;
             }
 
-            if (string.IsNullOrEmpty(textBox_Jezik.Text))
+            if (string.IsNullOrEmpty(odabraniJezik))
             {
-                if (Dispatcher != null)
-                    await Dispatcher?.InvokeAsync(() =>
-                    {
-                        NapomenaBox.Visibility = Visibility.Visible;
-                        NapomenaBox.Background = new SolidColorBrush(Color.FromArgb(0xFF, 0xEF, 0x3D, 0x4A));
-                        NapomenaBox.Text = "Unesite jezik za prikaz grupa.";
-                    });
+                MessageBox.Show("Odaberite jezik za prikaz grupa.");
                 return;
-            } else if (string.IsNullOrEmpty(textBox_NivoKursa.Text))
+
+            }else if (string.IsNullOrEmpty(odabraniNivo))
             {
-                if (Dispatcher != null)
-                    await Dispatcher?.InvokeAsync(() =>
-                    {
-                        NapomenaBox.Visibility = Visibility.Visible;
-                        NapomenaBox.Background = new SolidColorBrush(Color.FromArgb(0xFF, 0xEF, 0x3D, 0x4A));
-                        NapomenaBox.Text = "Unesite nivo kursa za prikaz grupa.";
-                    });
+                MessageBox.Show("Odaberite nivo kursa za prikaz grupa.");
                 return;
             }
 
             using (var ersteModel = new ErsteModel())
             {
-                var grupe = (from g in ersteModel.grupe join k in ersteModel.kursevi on g.KursId equals k.Id
+
+                var kursGrupa = (from g in ersteModel.grupe join k in ersteModel.kursevi on g.KursId equals k.Id
                              join j in ersteModel.jezici on k.JezikId equals j.Id
-                             where g.BrojClanova > 3 && k.DatumDo.CompareTo(DateTime.Now)>0
-                             && j.Naziv.Equals(textBox_Jezik.Text) && k.Nivo.Equals(textBox_NivoKursa.Text)
-                             select g).ToList();
-                foreach(var grupa in grupe)
+                             where g.BrojClanova>3 && k.DatumDo.CompareTo(DateTime.Now) > 0
+                             && j.Naziv.Equals(odabraniJezik) && k.Nivo.Equals(odabraniNivo)
+                             select new GrupaKursZapis {
+                                 Grupa = g,
+                                 Kurs = k,
+                                 Jezik = j 
+                             }).ToList();
+                foreach(var zapis in kursGrupa)
                 {
-                    GrupeDataGrid.Items.Add(grupa);
+                    if(!GrupeDataGrid.Items.Contains(zapis))
+                        GrupeDataGrid.Items.Add(zapis);
                 }
 
             }
         }
 
-        private async void Apply_Btn_Click(object sender, RoutedEventArgs e)
+        private  void Apply_Btn_Click(object sender, RoutedEventArgs e)
         {
+            string odabraniJezik = (string)chb_Jezik.SelectedItem;
+            string odabraniNivo = (string)chb_Nivo.SelectedItem;
 
             if (string.IsNullOrEmpty(textBox_Ime.Text) || string.IsNullOrEmpty(textBox_Prezime.Text)
                 || string.IsNullOrEmpty(textBox_Email.Text) || string.IsNullOrEmpty(textBox_BrojTelefona.Text)
-                    || string.IsNullOrEmpty(textBox_Jezik.Text) || string.IsNullOrWhiteSpace(textBox_NivoKursa.Text))
+                    || string.IsNullOrEmpty(odabraniJezik) || string.IsNullOrWhiteSpace(odabraniNivo))
             {
-                if (Dispatcher != null)
-                    await Dispatcher?.InvokeAsync(() =>
-                    {
-                        NapomenaBox.Visibility = Visibility.Visible;
-                        NapomenaBox.Background = new SolidColorBrush(Color.FromArgb(0xFF, 0xEF, 0x3D, 0x4A));
-                        NapomenaBox.Text = "Sva polja za unos moraju biti popunjena .";
-                    });
+                MessageBox.Show("Sva polja za unos moraju biti popunjena .");
                 return;
             }
 
-            if (!GrupeDataGrid.Items.IsEmpty && GrupeDataGrid.SelectedItems == null || GrupeDataGrid.SelectedItems.Count == 0)
+            if (!GrupeDataGrid.Items.IsEmpty && (GrupeDataGrid.SelectedItems == null || GrupeDataGrid.SelectedItems.Count == 0))
             {
-                if (Dispatcher != null)
-                    await Dispatcher?.InvokeAsync(() =>
-                    {
-                        NapomenaBox.Visibility = Visibility.Visible;
-                        NapomenaBox.Background = new SolidColorBrush(Color.FromArgb(0xFF, 0xEF, 0x3D, 0x4A));
-                        NapomenaBox.Text = "Izaberite grupu iz tabele.";
-                    });
+                MessageBox.Show("Izaberite grupu iz tabele.");
                 return;
             }
 
@@ -116,9 +107,12 @@ namespace Erste.Sluzbenik
 
                 if (!GrupeDataGrid.Items.IsEmpty)
                 {
-                    grupa g = (grupa)GrupeDataGrid.SelectedItem;
-                    p.grupe.Add(g);
-                    g.polaznici.Add(p);
+                    GrupaKursZapis zapis = (GrupaKursZapis)GrupeDataGrid.SelectedItem;
+                    grupa zapisGrupa = (from g in ersteModel.grupe where g.Id == zapis.Grupa.Id select g).First();
+                    p.grupe.Add(zapisGrupa);
+                    zapisGrupa.polaznici.Add(p);
+
+                    MessageBox.Show("Uspjeno dodan polaznik.");
                 }
                 else
                 {
@@ -126,13 +120,73 @@ namespace Erste.Sluzbenik
                     pnc.polaznik = p;
                     pnc.Id = p.Id;
 
+                    // RAZMISLI O OVOME, KAKO MAPIRATI POLAZNIKE PO KURSEVIMA RAZLICITIH DATUMA
                     var kursLista = from k in ersteModel.kursevi
                                     join j in ersteModel.jezici on k.JezikId equals j.Id
-                                    where k.Nivo.Equals(textBox_NivoKursa.Text) && j.Naziv.Equals(textBox_Jezik.Text)
+                                    where k.Nivo.Equals(odabraniNivo) && j.Naziv.Equals(odabraniJezik)
+                                        && k.DatumDo.CompareTo(DateTime.Now)>0
+                                    orderby k.DatumDo descending
                                     select k;
+
                     kurs kurs = kursLista.First();
-                    kurs.polaznici_na_cekanju.Add(pnc);
-                    pnc.kursevi.Add(kurs);
+                    IEnumerable<polaznik_na_cekanju> polazniciNaCekanjuZaTrazeniKurs = kursLista.SelectMany(k => k.polaznici_na_cekanju);
+                    if (polazniciNaCekanjuZaTrazeniKurs.Count() >= 2)
+                    {
+                        //nova grupa
+                        grupa g = new grupa
+                        {
+                            KursId = kurs.Id,
+                            BrojClanova = 0,
+                        };
+
+                        //unos podataka o novoj grupi
+                        UpisTerminaGrupe upisTermina = new UpisTerminaGrupe(g);
+                        upisTermina.ShowDialog();
+                        if (g.Naziv == null)
+                        {
+                            Task.Run(() => MessageBox.Show("Unesite naziv grupe.") );
+                            upisTermina = new UpisTerminaGrupe(g);
+                            upisTermina.ShowDialog();
+                        }
+
+                        //dobijanje ref na polaznike i polaznika na cekanju
+                        List<polaznik> polazniciNoveGrupe = new List<polaznik>();
+                        polazniciNoveGrupe.Add(p);
+                        foreach (polaznik_na_cekanju p_na_c in polazniciNaCekanjuZaTrazeniKurs)
+                        {
+                            polazniciNoveGrupe.Add(p_na_c.polaznik);
+                        }
+                        
+                        //brisanje korisnika na cekanju i veza s kursevima
+                        foreach (polaznik_na_cekanju p_na_c in polazniciNaCekanjuZaTrazeniKurs)
+                        {
+                            kurs kurs_za_p_na_c = p_na_c.kursevi.First(k => k.Nivo.Equals(odabraniNivo) &&
+                                k.jezik.Naziv.Equals(odabraniJezik));
+                            kurs_za_p_na_c.polaznici_na_cekanju.Remove(p_na_c);
+                            p_na_c.kursevi.Remove(kurs_za_p_na_c);
+                            p_na_c.polaznik.polaznik_na_cekanju = null;
+                        }
+
+                        //dodavanje polaznika u grupu
+                        foreach (polaznik p_u_g in polazniciNoveGrupe)
+                        {
+                            p_u_g.grupe.Add(g);
+                            g.polaznici.Add(p_u_g);
+                        }
+
+                        //dodavanje grupe u tabelu
+                        ersteModel.grupe.Add(g);
+
+                        MessageBox.Show("Polaznik uspjesno ubacen u grupu.Polaznici na cekanju za odabrani kurs i nivo su takodje uspjesno ubaceni u grupu.");
+
+                    }
+                    else
+                    {
+                        kurs.polaznici_na_cekanju.Add(pnc);
+                        pnc.kursevi.Add(kurs);
+
+                        MessageBox.Show("Polaznik dodat na listu cekanja za odabrani kurs i jezik.");
+                    }   
                 }
 
                 ersteModel.SaveChanges();
@@ -150,5 +204,14 @@ namespace Erste.Sluzbenik
         {
         
         }
+    }
+
+    public class GrupaKursZapis {
+
+        public grupa Grupa { get; set; }
+        public kurs Kurs { get; set; }
+        public jezik Jezik { get; set; }
+
+
     }
 }
